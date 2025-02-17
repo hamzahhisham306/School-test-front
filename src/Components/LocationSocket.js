fbf0e2b606e8e0e1e7a72de69be9180fb8d65fa7
Hamzah Aldaamas <86842077+hamzahhisham306@users.noreply.github.com>
2025-02-17 20:01:13 +0300

    Update LocationSocket.js

--- src/Components/LocationSocket.js
+++ src/Components/LocationSocket.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 import io from "socket.io-client";
 
